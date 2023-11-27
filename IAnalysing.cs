d2ea5c6393c3cb9bd4e70ca62b970407cd14006e
DashaDudk <151950266+DashaDudk@users.noreply.github.com>
2023-11-27 17:12:09 +0200

    Add files via upload

---
+++ IAnalysing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InfoSystem
+{
+    interface IAnalysing
+    {
+        void AnalisingMethod(string[] restrictions, string filePath, DataGridView dataGridView1);
+    }
+}
